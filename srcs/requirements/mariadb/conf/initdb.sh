816686a65e91f87db3998637f161389ec5c60431
aleoru <aleoru0022@gmail.com>
2024-02-21 14:51:52 -0500

    Mariadb working

--- srcs/requirements/mariadb/conf/initdb.sh
+++ srcs/requirements/mariadb/conf/initdb.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-service mysql start;
+service mariadb start
+sleep 10
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PWD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
-mysql -e "FLUSH PRIVILEGES;" >> db.sql
-
-mysqladmin -u root -p$SQL_ROOT_PWD shutdown
-exec mysqld_safe
+mysql -e "FLUSH PRIVILEGES;"
