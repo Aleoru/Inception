8233bcac463fd9afc86b2ae7d6fcf20f108dd803
aleoru <aleoru0022@gmail.com>
2024-02-23 14:06:05 -0500

    installed wordpress cli

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-wp-config create --allow-root \
+cd /var/www/html
+
+wp core download --allow-root
+
+wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	--dbhost=mariadb:3306 --path='/var/www/html'
