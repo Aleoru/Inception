1b195a378a919b2908dbd0f72f8aa69e7707594a
aleoru <aleoru0022@gmail.com>
2024-02-24 15:08:23 -0500

    Trying to connect mariadb and wordpress

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,8 +4,16 @@ cd /var/www/html
 
 wp core download --allow-root
 
-wp config create --allow-root \
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/html'
+#mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_PWD --dbhost=mariadb --path='/var/www/html'
+
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp theme install kubio --activate --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir /run/php
+
+/usr/sbin/php-fpm7.4 -F
