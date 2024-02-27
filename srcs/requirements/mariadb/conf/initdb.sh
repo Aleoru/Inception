f767438edd36697d0a8ac4212afc0ad0d3534681
aleoru <aleoru0022@gmail.com>
2024-02-27 15:09:06 -0500

    All working

--- srcs/requirements/mariadb/conf/initdb.sh
+++ srcs/requirements/mariadb/conf/initdb.sh
@@ -3,8 +3,8 @@
 service mariadb start
 sleep 10
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DB};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DB}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PWD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
 mysql -e "FLUSH PRIVILEGES;"
