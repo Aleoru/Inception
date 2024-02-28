567812f5ed284f65596acda575581673cce94238
aleoru <aleoru0022@gmail.com>
2024-02-28 14:25:22 -0500

    Delete one commented line

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,8 +4,6 @@ cd /var/www/html
 
 wp core download --allow-root
 
-#mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
 wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_PWD --dbhost=mariadb --path='/var/www/html'
 
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
