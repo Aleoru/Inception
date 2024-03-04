59748e77213b0a0506c01ae8d799cd715d7076e5
aleoru <aleoru0022@gmail.com>
2024-03-04 14:53:52 -0500

    CSS working, mariadb socket error still there

--- srcs/requirements/mariadb/conf/create.sh
+++ srcs/requirements/mariadb/conf/create.sh
@@ -3,6 +3,6 @@
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
 mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PWD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
-mysql -e "FLUSH PRIVILEGES;"
+#mysql -e "FLUSH PRIVILEGES;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
 mariadb-admin -u root -p${SQL_ROOT_PWD} -hlocalhost shutdown
